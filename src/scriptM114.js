96e2e4f273d4cc1bdb8ed6d822d1ec31ccf9d3a6
Jovibess <joeydecroix.pro@gmail.com>
2025-03-07 16:28:15 +0100

    Update prettier

--- src/scriptM114.js
+++ src/scriptM114.js
@@ -1,3 +1,8 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
 // Header
 
 const menuburger = document.querySelector(".menu-burger");
@@ -6,122 +11,112 @@ const menu2 = document.querySelector(".menu2");
 let isMenuOpen = false; // Suivi de l'état du menu
 
 menuburger.addEventListener("click", () => {
-    if (!isMenuOpen) {
-        menu2.style.display = "flex"; // Affiche le menu avant de déclencher l'animation
-        menu2.classList.remove("pop-menu-exit");
-        void menu2.offsetWidth; // Réinitialise l'état de l'animation
-        menu2.classList.add("pop-menu-enter");
-    } else {
-        // Si le menu est ouvert, appliquer l'animation de sortie
-        menu2.classList.remove("pop-menu-enter");
-        void menu2.offsetWidth; // Réinitialise l'état de l'animation
-        menu2.classList.add("pop-menu-exit");
-    }
-    isMenuOpen = !isMenuOpen;
+  if (!isMenuOpen) {
+    menu2.style.display = "flex"; // Affiche le menu avant de déclencher l'animation
+    menu2.classList.remove("pop-menu-exit");
+    void menu2.offsetWidth; // Réinitialise l'état de l'animation
+    menu2.classList.add("pop-menu-enter");
+  } else {
+    // Si le menu est ouvert, appliquer l'animation de sortie
+    menu2.classList.remove("pop-menu-enter");
+    void menu2.offsetWidth; // Réinitialise l'état de l'animation
+    menu2.classList.add("pop-menu-exit");
+  }
+  isMenuOpen = !isMenuOpen;
 });
 
 // Lors de la fin de l'animation, cacher complètement le menu si c'est une sortie
 menu2.addEventListener("animationend", (event) => {
-    if (event.animationName === "slideOut") {
-        menu2.style.display = "none"; // Cache complètement après l'animation de sortie
-    }
+  if (event.animationName === "slideOut") {
+    menu2.style.display = "none"; // Cache complètement après l'animation de sortie
+  }
 });
 
-
 // Section 1
-const section = document.querySelector('.sectionOne');
+const section = document.querySelector(".sectionOne");
 
-document.querySelectorAll('.sectionOne').forEach(element => {
-    ["mouseenter", "touchmove"].forEach(eventType => {
-        element.addEventListener(eventType, () => {
-            const mockup = document.querySelector('.mockup1M114');
+document.querySelectorAll(".sectionOne").forEach((element) => {
+  ["mouseenter", "touchmove"].forEach((eventType) => {
+    element.addEventListener(eventType, () => {
+      const mockup = document.querySelector(".mockup1M114");
 
-            // Ajoute la classe "animated" pour rendre l'état persistant
-            mockup.addEventListener('animationend', () => {
-                section.classList.add('animated');
-            }, { once: true }); // "once" garantit que l'écouteur se retire après la première exécution
-        });
+      // Ajoute la classe "animated" pour rendre l'état persistant
+      mockup.addEventListener(
+        "animationend",
+        () => {
+          section.classList.add("animated");
+        },
+        { once: true }
+      ); // "once" garantit que l'écouteur se retire après la première exécution
     });
+  });
 });
 
 // section 3
 
 const panels = document.querySelectorAll(".panel");
 panels.forEach((panel) => {
-    panel.addEventListener("click", () => {
-        removeActiveClasses();
-        panel.classList.add("active");
-    });
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
 });
 
 function removeActiveClasses() {
-    panels.forEach((panel) => {
-        panel.classList.remove("active");
-    });
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
-;
-
 // // Section 5
 
-const section5 = document.querySelector('.sectionFive');
-const mockupIphone = document.querySelector('.mockupIphone');
-const mockupTablette = document.querySelector('.mockupTablette');
+const section5 = document.querySelector(".sectionFive");
+const mockupIphone = document.querySelector(".mockupIphone");
+const mockupTablette = document.querySelector(".mockupTablette");
 
-["mouseenter", "touchmove"].forEach(eventType => {
-    section5.addEventListener(eventType, () => {
-        mockupIphone.classList.add('animated1');
-        mockupTablette.classList.add('animated2');
-    }, { once: true }); // L'événement ne s'exécutera qu'une seule fois
+["mouseenter", "touchmove"].forEach((eventType) => {
+  section5.addEventListener(
+    eventType,
+    () => {
+      mockupIphone.classList.add("animated1");
+      mockupTablette.classList.add("animated2");
+    },
+    { once: true }
+  ); // L'événement ne s'exécutera qu'une seule fois
 });
 
 // Circle effect
 
+let circle = document.querySelector(".circle");
+circle.addEventListener("click", function () {
+  for (let i = 0; i < 80; i++) {
+    let particles = document.createElement("i");
+    particles.classList.add("particles");
+    let randomX = (Math.random() - 0.5) * window.innerWidth;
+    let randomY = (Math.random() - 0.5) * window.innerHeight;
 
-let circle = document.querySelector('.circle');
-circle.addEventListener('click', function(){
-    for(let i = 0; i < 80; i++){
-        let particles = document.createElement('i');
-        particles.classList.add('particles');
-        let randomX = (Math.random() - 0.5) * window.innerWidth;
-        let randomY = (Math.random() - 0.5) * window.innerHeight;
+    particles.style.setProperty("--x", randomX + "px");
+    particles.style.setProperty("--y", randomY + "px");
 
-        particles.style.setProperty('--x' , randomX+'px');
-        particles.style.setProperty('--y' , randomY+'px');
+    let randomSize = Math.random() * window.innerWidth * 0.03;
+    particles.style.width = randomSize + "px";
+    particles.style.height = randomSize + "px";
 
-        let randomSize = Math.random() * window.innerWidth * 0.03;
-        particles.style.width = randomSize + 'px';
-        particles.style.height = randomSize + 'px';
+    let duration = Math.random() * 3 + 2;
 
-        let duration = Math.random() * 3 + 2;
-        
-        particles.style.animation = `animate ${duration}s ease forwards`;
+    particles.style.animation = `animate ${duration}s ease forwards`;
 
-        document.querySelector('.circleEffect').appendChild(particles);
+    document.querySelector(".circleEffect").appendChild(particles);
 
-        setTimeout(function(){
-            particles.remove(); 
-        },5000);
-    }
-})
-
+    setTimeout(function () {
+      particles.remove();
+    }, 5000);
+  }
+});
 
 // Footer
 
-const marquee = document.querySelector('.marquee-content');
-        
+const marquee = document.querySelector(".marquee-content");
+
 // Duplique le contenu pour éviter le saut
 const clone = marquee.cloneNode(true);
 marquee.parentElement.appendChild(clone);
-
-
-
-
-
-
-
-
-
-
-
-
-
